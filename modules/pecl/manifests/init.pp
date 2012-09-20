442f43cbf8b4a4475af534572940a280a62cc86b
John Le Drew <jp@antz29.com>
2012-09-20 16:21:38 +0100

    add build-essential package

--- modules/pecl/manifests/init.pp
+++ modules/pecl/manifests/init.pp
@@ -1,13 +1,17 @@
 class pecl {
+
+	package { 'build-essential':
+	  ensure => installed
+	}
 	
 	package { 'php-pear':
 	  ensure => installed,
-	  require => Package['php5-cli','php5-fpm']
+	  require => Package['php5-cli','php5-fpm','build-essential']
 	}
 
 	package { 'php5-dev':
 	  ensure => installed,
-	  require => Package['php5-cli','php5-fpm']
+	  require => Package['php5-cli','php5-fpm','build-essential']
 	}	
 
 }
